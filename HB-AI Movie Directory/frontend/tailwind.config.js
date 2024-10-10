5b9383c5201b9ff55a84178855cdceeb7613989d
Htet Yamin Ei 47743220 <htetyamin.ei@students.mq.edu.au>
2024-10-10 22:25:39 +1100

    Implemented light/dark mode and logo

--- HB-AI Movie Directory/frontend/tailwind.config.js
+++ HB-AI Movie Directory/frontend/tailwind.config.js
@@ -1,16 +1,13 @@
 /** @type {import('tailwindcss').Config} */
+
 export default {
-	content: [
-		"./index.html",
-		"./src/**/*.{js,ts,jsx,tsx}",
-	],
-	theme: {
-		extend: {
-			fontFamily: {
-				sans: ['Roboto', 'sans-serif'],
-			},
-		},
-	},
-	darkMode: 'class', // enables dark mode via a class
-	plugins: [],
-}
+  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
+  theme: {
+    fontFamily: {
+      sans: ['Roboto', 'sans-serif'],
+    },
+    extend: {},
+  },
+  darkMode: 'class', // enables dark mode via a class
+  plugins: [],
+};
